8ad1971ae37e1f9201f3856a3a438c0389b929bb
anshukumar172 <133948976+anshukumar172@users.noreply.github.com>
2025-07-03 16:47:46 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ print(get_requirements())
 setup(
     name="AI-TRAVEL-PLANNER",
     version="0.0.1",
-    author="sunny savita",
-    author_email="snshrivas3365@gmail.com",
+    author="Anshu Kumar",
+    author_email="kumaranshu9410@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements()
 )
